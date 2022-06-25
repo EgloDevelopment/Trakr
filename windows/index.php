a28a93d827c286a8c91f7532812e673c8da93459
Eglo <105808341+EgloDevelopment@users.noreply.github.com>
2022-06-25 16:12:02 -0600

    Fixed a stupid mistake

--- windows/index.php
+++ windows/index.php
@@ -179,12 +179,12 @@ function getIP()
 define('DB_SERVER', 'database-1.caxwicrpfcuz.us-east-2.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'toasteriscool');
-define('DB_NAME', 'Tracker');*/
+define('DB_NAME', 'tracker');*/
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'Tracker');
+define('DB_NAME', 'tracker');
 
 $mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
@@ -201,7 +201,7 @@ if ($conn->connect_error) {
 $key = clear($_GET['key']);
 $auth = clear($_GET['auth']);
 
-$sql = "SELECT * FROM Users WHERE apikey = '$key'";
+$sql = "SELECT * FROM users WHERE apikey = '$key'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -225,7 +225,7 @@ if ($result->num_rows > 0) {
         $memUsage = getServerMemoryUsage(false);
         $ram = getNiceFileSize($memUsage["total"] - $memUsage["free"]);
         $time = (number_format(microtime(true) - $start_time, 2));
-        $sql = "INSERT INTO `Data`(`ip`, `country`, `device`, `loadtime`, `servercpu`, `serverram`, `apiauth`) VALUES ('$ip','$country','$device','$time','$cpu','$ram','$auth')";
+        $sql = "INSERT INTO `data`(`ip`, `country`, `device`, `loadtime`, `servercpu`, `serverram`, `apiauth`) VALUES ('$ip','$country','$device','$time','$cpu','$ram','$auth')";
         $conn->query($sql);
         //$conn->query($sql3);
     } else {
