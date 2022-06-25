0d951b2c4a3c2c16f27f31250087e1137ea41fc0
Eglo <105808341+EgloDevelopment@users.noreply.github.com>
2022-06-25 16:15:00 -0600

    Some simplification stuff

--- linux/index.php
+++ linux/index.php
@@ -76,24 +76,7 @@ function getIP()
     return 'Country: ' . $country;
 }
 
-
-
-
-define('DB_SERVER', 'database-1.caxwicrpfcuz.us-east-2.rds.amazonaws.com');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'toasteriscool');
-define('DB_NAME', 'tracker');
-/*
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'tracker');*/
-
-$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if (!$mysql_db) {
-    die("Error: Unable to connect " . $mysql_db->connect_error);
-}
+require_once('../config/config.php');
 
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
